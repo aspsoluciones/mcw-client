4b86f746c636fc7e1412a4bc2f20aee8de9d1564
Epotignano <epotignano@gmail.com>
2016-04-11 15:30:19 -0300

    refresh_token service integration. Need to apply the solution for all the application context

--- src/constants/Commons.js
+++ src/constants/Commons.js
@@ -4,3 +4,4 @@
 export const ApiRef = 'http://desa-prog03.miclinicaweb.com/WebApi';
 export const UidRef= 'mcw_uid';
 export const TokenRef= 'mcw_token';
+export const RefreshTokenRef= 'mcw_refresh_token';
