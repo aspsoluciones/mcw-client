4f9e471bb37d3abc5e86cf998715b702b2be3fa7
Epotignano <epotignano@gmail.com>
2016-04-13 00:56:27 -0300

    Card is taking shape!

--- src/components/DoctorProfileCard.js
+++ src/components/DoctorProfileCard.js
@@ -3,37 +3,41 @@
  */
 
 import React from 'react';
-export function DoctorProfileCard(doctorData) {
-  return <div className="ui stackable column centered grid">
-    <div className="ui row">
-      <div className="ui two column centered grid">
-          <div className="column">
-            <image className="ui small image circular" src="../assets/DrChapatin.jpg"/>
-          </div>
-          <div className="column">
-            <h1> { doctorData.lastName } { doctorData.firstName }</h1>
-            <h5>
-              {doctorData.categories[0]}, {doctorData.categories[1]}
-            </h5>
-          </div>
-      </div>
 
-      <div className="ui four wide column">
-        <h4>Something else</h4>
-      </div>
-    </div>
-    <div className="ui row">
-      <div className="ui four wide column">
 
-      </div>
-      <div className="ui twelve wide column">
-        <h5>
-          { doctorData.selfReview }
-        </h5>
-      </div>
-    </div>
-
+function categories(focuses) {
+  return <div>
+    Enfoques:
   </div>
+}
+
+
+export function DoctorProfileCard(doctorData) {
+  return  <div className="ui vertical stripe segment">
+            <div className="ui middle aligned grid container">
+              <div className="ui row segment no-padded-segment">
+                <div className="six wide column no-padded">
+                  <div class="ui column">
+                    <image className="ui image medium" src="../assets/DrChapatin.jpg"/>
+                  </div>
+                </div>
+                <div className="eight wide right floated column">
+                  <div className="ui column">
+                    <h3 className="ui header">Dr. {doctorData.lastName} {doctorData.firstName}</h3>
+                  </div>
+                  <div className="ui column">
+                    <h5 className="ui header">{doctorData.categories[0]}, {doctorData.categories[1]}</h5>
+                  </div>
+                  <div className="ui column">
+                    <span> Enfoques:</span><a className="ui blue image label">
+                    <img src="../assets/DrChapatin.jpg"/>
+                    { doctorData.focuses[0]}
+                  </a>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
 }
 
 
