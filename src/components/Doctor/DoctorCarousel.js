c222c41797cd82e2c678399fc541553a91757a0f
Epotignano <epotignano@gmail.com>
2016-05-28 15:24:12 -0300

    Add chevron to carousel

--- src/components/Doctor/DoctorCarousel.js
+++ src/components/Doctor/DoctorCarousel.js
@@ -7,6 +7,60 @@ var Carousel = require('nuka-carousel');
 var style = {
         backgroundImage: "url(//fi.realself.com/hero/3b4b9f8ee43989919a60d83fb43a3999/a/e/c/userimage-968246.jpg)"
     };
+    
+var decorators = [{
+  component: React.createClass({
+    render() {
+      return (
+        <button
+          style={this.getButtonStyles(this.props.currentSlide === 0)}
+          onClick={this.props.previousSlide}>
+          <i className="large angle left icon"></i>
+        </button>
+      )
+    },
+    
+    getButtonStyles(disabled) {
+        return {
+          border: 0,
+          background: 'rgba(0,0,0,0.4)',
+          color: 'white',
+          padding: 10,
+          outline: 0,
+          opacity: disabled ? 0.3 : 1,
+          cursor: 'pointer'
+      }
+    }
+  }),
+  position: 'CenterLeft'
+},
+{
+  component: React.createClass({
+    render() {
+      return (
+        <button
+          style={this.getButtonStyles(this.props.currentSlide + this.props.slidesToScroll >= this.props.slideCount)}
+          onClick={this.props.nextSlide}>
+          <i className="large angle right icon"></i>
+        </button>
+      )
+    },
+    
+    getButtonStyles(disabled) {
+        return {
+          border: 0,
+          background: 'rgba(0,0,0,0.4)',
+          color: 'white',
+          padding: 10,
+          outline: 0,
+          opacity: disabled ? 0.3 : 1,
+          cursor: 'pointer'
+      }
+    }
+  }),
+  position: 'CenterRight'
+}
+];
 
 const DoctorCarousel = React.createClass({
   mixins: [Carousel.ControllerMixin],
@@ -15,7 +69,7 @@ const DoctorCarousel = React.createClass({
     return (
       <div id="carousel">
         <div className="carousel-outer">
-          <Carousel className="doctorCarousel">
+          <Carousel className="doctorCarousel" decorators={decorators}>
             <div style={{backgroundImage: "url(//fi.realself.com/hero/3b4b9f8ee43989919a60d83fb43a3999/a/e/c/userimage-968246.jpg)"}} className="carouselImage"></div>
             <div style={{backgroundImage: "url(//fi.realself.com/hero/3b4b9f8ee43989919a60d83fb43a3999/a/e/c/userimage-968246.jpg)"}} className="carouselImage"></div>
           </Carousel>
