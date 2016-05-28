57521495452b7f846c5b814b51263179a8985c68
Epotignano <epotignano@gmail.com>
2016-05-28 13:49:38 -0300

    General information mock

--- src/components/Doctor/DoctorGeneralInformation.js
+++ src/components/Doctor/DoctorGeneralInformation.js
@@ -3,18 +3,24 @@ import React from 'react';
 const DoctorGeneralInformation = ( { doctor } ) => {
     
     return(
-        
-        
-        <table className="ui table">
-            <tbody>
-                <tr>
-                    <td>Idiomas</td>
-                    <td>Mock Español</td>        
-                </tr>
-            </tbody>
-            
-        </table>
-        
+        <div>
+            <h3 className="color-mcwDark" style={{marginBottom: 0}}>
+                <i className="doctor icon"></i>Información general
+            </h3>
+            <table className="ui table">
+                <tbody>
+                    <tr>
+                        <td>Sobre mi</td>
+                        <td>Sé que puedo hacer una diferencia en la vida de mis pacientes</td>        
+                    </tr>
+                    <tr>
+                        <td>Idiomas</td>
+                        <td>English, Español</td>        
+                    </tr>
+                </tbody>
+                
+            </table>
+        </div>
     )
     
 }
