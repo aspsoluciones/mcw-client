bb0bbdf96dd2b13b26533e917f7deabcef2588d1
Epotignano <epotignano@gmail.com>
2016-08-04 10:35:15 -0300

    Cambio

--- src/components/WeekDisplayer.js
+++ src/components/WeekDisplayer.js
@@ -132,7 +132,7 @@ class WeekDisplayer extends Component {
 
     dispatch(TakeAppointment({appointment}));
     router.push({
-      pathname: '/' + doctorUsername + '/appointment/checkout'
+      pathname: '/webui/' + doctorUsername + '/appointment/checkout'
     });
   }
 
